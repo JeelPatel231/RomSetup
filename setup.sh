e838c43df6b48e513cf20a7db05c557e91c17db5
Jeel <33726155+JeelPatel231@users.noreply.github.com>
2020-05-06 16:13:45 +0530

    another source for CLI -

--- setup.sh
+++ setup.sh
@@ -13,7 +13,15 @@ sudo service xrdp restart
 sudo passwd $uservar
 
 echo "Setting up CLI"
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install azure-cli
 az login
 az vm open-port --resource-group $myResourceGroup --name $myVM --port 3389
 
