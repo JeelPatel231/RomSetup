4d45bfdddf624d88f4ad91c3ff6a26557aed0a5a
Jeel <33726155+JeelPatel231@users.noreply.github.com>
2020-05-06 15:49:35 +0530

    added python-pip to the list

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ az login
 az vm open-port --resource-group $myResourceGroup --name $myVM --port 3389
 
 echo "Installing Prerequisites"
-sudo apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev gedit -y
+sudo apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev gedit python-pip -y
 
 echo "Installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
