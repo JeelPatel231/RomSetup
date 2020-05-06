4ee362fb7d8d5cb0a50d3de638b0e9d0a8369f39
Jeel <33726155+JeelPatel231@users.noreply.github.com>
2020-05-06 14:11:35 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+echo "Installing Prerequisites"
+sudo apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev gedit -y
+
+echo "Installing Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+echo "Installing and configuring REPO"
+mkdir -p ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+source ~/.profile
